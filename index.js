00ded4d7296274c547ee7d1596885d72080aea2c
codewithnanu <gauranisha3@gmail.com>
2026-03-11 15:54:41 +0530

    prisma

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const prisma = require("../prisma/client");
+const prisma = require("./prisma/client");
 
 
 const app = express();
